ce6a419a45bc75738003afcc883096fab6f61958
burakcanturk12 <burakcanturk12@gmail.com>
2026-04-29 19:40:25 +0300

    update

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,4 @@ import "dotenv/config";
 
 export default ({ config }: any) => ({
   ...config,
-  orientation: "default", // genel
-
-  android: {
-    ...config.android,
-    orientation: "portrait", // sadece Android
-  },
-
-  ios: {
-    ...config.ios,
-    orientation: "landscape", // sadece iOS
-  },
 });
